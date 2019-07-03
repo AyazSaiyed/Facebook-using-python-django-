5d7e32810df52e54c8771510f707cabf7fb83148
Ayaz Saiyed <49748587+AyazSaiyed@users.noreply.github.com>
2019-07-03 11:27:56 +0530

    Update settings.py

--- postupload/postupload/settings.py
+++ postupload/postupload/settings.py
@@ -115,7 +115,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
