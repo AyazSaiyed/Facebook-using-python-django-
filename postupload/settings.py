612a19dfb513ad5b719986a3e4f35a291dc8b7cb
Ayaz Saiyed <49748587+AyazSaiyed@users.noreply.github.com>
2019-07-03 14:24:23 +0530

    Update settings.py

--- postupload/settings.py
+++ postupload/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'postuploadapp',
+    'postupload.postuploadapp',
 ]
 
 MIDDLEWARE = [
